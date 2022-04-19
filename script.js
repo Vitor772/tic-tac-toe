633f01b4f653f366777a947a4cb8c08b1cd951fd
Vitor772 <vitor989santos@gmail.com>
2022-04-18 23:51:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const celulas = document.querySelectorAll(".celula");
+let checarTurno = true;
+
+const JOGADOR_X = "X";
+const JOGADOR_O = "O";
+
+document.addEventListener("click", (event) =>{
+    if(event.target.matches(".celula")) {
+    jogar(event.target.id);
+    }
+});
+
+function jogar(id){
+    const celula = document.getElementById(id);
+    turno = checarTurno ? JOGADOR_X : JOGADOR_O;
+    celula.textContent = turno;
+    checarTurno = !checarTurno;
+    checarVencedor(turno);
+
+}
