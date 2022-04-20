4a1122d3a635007b24923d470ca1881a613b3252
Vitor772 <vitor989santos@gmail.com>
2022-04-19 22:18:02 -0300

    update/19/04/2022

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ let checarTurno = true;
 
 const JOGADOR_X = "X";
 const JOGADOR_O = "O";
-
+const COMBINACAOES = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 document.addEventListener("click", (event) =>{
     if(event.target.matches(".celula")) {
     jogar(event.target.id);
@@ -14,7 +23,34 @@ function jogar(id){
     const celula = document.getElementById(id);
     turno = checarTurno ? JOGADOR_X : JOGADOR_O;
     celula.textContent = turno;
-    checarTurno = !checarTurno;
+    celula.classList.add(turno)
     checarVencedor(turno);
+    
+}
 
+function checarVencedor(turno){
+    const vencedor = COMBINACAOES.some((comb) => {
+        return comb.every((index) => {
+            return celulas[index].classList.contains(turno)
+
+        })
+
+    });
+
+        if (vencedor){
+            encerraJogo(turno);
+        } else if (checarEmpate()) {
+            encerraJogo();
+        } else {
+            checarTurno = !checarTurno;
+            
+    }
+}
+
+function encerraJogo(vencedor = null) {
+    if (vencedor){
+        console.log("vencedor: " + vencedor)
+    } else {
+        console.log("Empate")
+    }
 }
